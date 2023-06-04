62f2dee788afd735634a44097b4815cb3f51177d
rodrigocb23 <rodrigocb2012@gmail.com>
2023-06-04 13:16:31 -0300

    first commit

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+import questions from "../../../assets/data/quizz_questions.json"
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(questions){
+      this.finished = false
+      this.title = questions.title
+
+      this.questions = questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = questions.results[finalAnswer as keyof typeof questions.results ]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+        if(
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ){
+          return previous
+        }else{
+          return current
+        }
+    })
+
+    return result
+  }
+}
